7e60a47e07dfc14d4a9c0b943f55d3c59e8a7f6b
tgh15 <muhteguhadhiputra15@gmail.com>
2023-08-03 21:53:38 +0800

    show specific cluster

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-data_awal = ""
-data_hasil = ""
 dataframe = pd.DataFrame(pd.read_excel("data dosen.xlsx"))
+data_awal = dataframe.copy()
+data_hasil = dataframe.copy()
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
